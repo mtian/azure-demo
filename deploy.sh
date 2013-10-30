4c15477950e03cae7c78cbec721a34aba464cb6e
Jose Diaz <jose.a.diaz@aguaseca.com>
2013-10-30 15:31:51 -0700

    updated path to Bower

--- deploy.sh
+++ deploy.sh
@@ -76,7 +76,7 @@ if [[ ! -n "$BOWER_CMD" ]]; then
     BOWER_CMD="bower"
   else
     # In case we are running on kudu service this is the correct location of kuduSync
-    BOWER_CMD="c:/DWASFiles/Sites/animated-octo/AppData/npm/node_modules/bower/bin/bower"
+    BOWER_CMD="$AppData/npm/node_modules/bower/bin/bower"
   fi
 fi
 
